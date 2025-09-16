ba4a92765c630ddb380408ae98524ed08f2c1c63
CrisMarin1211 <alca81411@gmail.com>
2025-09-15 20:57:42 -0500

    feat

--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { UserContext } from "./UserContext";
 
-// Aquí va solo el componente provider
+
 export const UserProvider = ({ children }) => {
   const [name, setName] = useState("Anonymous");
 
